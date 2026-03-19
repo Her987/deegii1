5e6958febbd12b9f2ed10ca80c00aad725b622d2
Batorgil <batorgil056@gmail.com>
2026-03-19 08:42:58 -0700

    fix deploy structure

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,10 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-<<<<<<< HEAD
-    serviceWorkerVersion: "2361230692" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
-=======
-    serviceWorkerVersion: "3013445876" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
->>>>>>> 2986587 (deploy web)
+    serviceWorkerVersion: "3388387438" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
